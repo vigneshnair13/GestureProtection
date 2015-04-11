a70f5b234483c888a6797f75b327139766395284
vigneshnair13 <vignesh.nair@vamsystems.com>
2015-04-11 19:05:56 -0400

    Create dangeremail.sh

---
+++ dangeremail.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "EMERGENCY: Patient 1 requires assistance immediately. Room number 24." | mail -s "IMPORTANT: Patient 1 requires assistance immediately" patientassistanceservice@gmail.com

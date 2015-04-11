eba2870262067ace7cb07c2bf5aa1f21f0e6712d
vigneshnair13 <vignesh.nair@vamsystems.com>
2015-04-11 18:08:59 -0400

    Created danger.py

---
+++ danger.py
@@ -0,0 +1,19 @@
+import time
+import RPi.GPIO as io
+io.setmode(io.BCM)
+
+
+RED_LED_1 = 24
+RED_LED_2 = 25
+
+
+io.setup(RED_LED_1, io.OUT)
+io.setup(RED_LED_2, io.OUT)
+
+while True:
+	io.output(RED_LED_1, True);
+	io.output(RED_LED_2, True);
+  os.system("dangeremail.sh")
+  os.system("dangertext.sh")
+GPIO.cleanup()
+		
